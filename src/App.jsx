71983bf5597fd0f964f7438608a84bf925da4c05
aadil <adilev2000@gmail.com>
2024-07-13 14:39:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-700">App.js</h1>
+    </>
+  )
+}
+
+export default App
+
