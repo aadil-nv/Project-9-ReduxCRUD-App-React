1d771470a2cd35553f27d0ec09d2a6a2226cb005
aadil <adilev2000@gmail.com>
2024-07-13 16:10:32 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import Signin from "./Pages/Signin";
 import Signup from "./Pages/Signup";
+import Header from "./Components/Header";
 BrowserRouter;
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route  path="/"  element={<Home/>} />
         <Route  path="/about"  element={<About/>} />
